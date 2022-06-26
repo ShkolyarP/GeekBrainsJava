649021d61cc5b57a0d1080de524b3b5ac86d11ce
Vladimir Palt <paltvlad@gmail.com>
2022-06-26 12:19:13 +0300

    HomeWork 11, GeekBrains, semester 1

---
+++ src/main/java/HomeWork11/Task2.java
@@ -0,0 +1,13 @@
+package HomeWork11;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Phonebook.add("Ivanov", "775-35-23");
+        Phonebook.add("Petrov", "224-65-56");
+        Phonebook.add("Petrov", "224-65-57");
+
+        Phonebook.get("Petrov");
+
+        
+    }
+}
